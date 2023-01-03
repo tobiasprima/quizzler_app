414893e7ada4a53ff8235c7aea1f4a25d85cf09e
Tobey <primatjahja@gmail.com>
2023-01-03 22:45:37 +0800

    Set the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -31,59 +31,70 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List scoreKeeper = [];
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            flex: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  'This is where the question text will go.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 25.0),
-                ),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.green),
-                ),
-                onPressed: () {
-                  print("The user picked true");
-                },
-                child: Text(
-                  'True',
-                  style: TextStyle(color: Colors.white, fontSize: 30.0),
-                ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
+              ),
+              onPressed: () {
+                print("The user picked true");
+              },
+              child: Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
               ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.red),
-                ),
-                onPressed: () {
-                  print("The user picked false");
-                },
-                child: Text(
-                  'False',
-                  style: TextStyle(color: Colors.white, fontSize: 25.0),
-                ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.red),
+              ),
+              onPressed: () {
+                print("The user picked false");
+              },
+              child: Text(
+                'False',
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
             ),
-          )
-        ]);
+          ),
+        ),
+        Row(
+          children: [
+            Icon(Icons.check, color: Colors.green),
+            Icon(Icons.check, color: Colors.green),
+            Icon(Icons.close, color: Colors.red),
+            Icon(Icons.close, color: Colors.red),
+            Icon(Icons.close, color: Colors.red),
+          ],
+        )
+      ],
+    );
   }
 }
