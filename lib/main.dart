c1f4f9ce5e1f21ce20abc69426c94d811d611823
Tobey <primatjahja@gmail.com>
2023-01-03 23:01:56 +0800

    Set the questions

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,13 @@ class _QuizPageState extends State<QuizPage> {
     Icon(Icons.close, color: Colors.red),
     Icon(Icons.close, color: Colors.red),
   ];
+  List<String> questions = [
+    'You can lead a cow down stairs but not upstairs.',
+    'Approximately one quarter of human bones are in the feet',
+    'A slug\'s blood is green.'
+  ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -50,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
@@ -66,14 +73,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 print("The user picked true");
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                });
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                    questionNumber++;
+                  },
+                );
               },
               child: Text(
                 'True',
@@ -91,6 +101,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 print("The user picked false");
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                    questionNumber++;
+                  },
+                );
               },
               child: Text(
                 'False',
